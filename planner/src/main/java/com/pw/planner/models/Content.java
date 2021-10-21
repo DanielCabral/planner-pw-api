baa711218631556ef7bca526e0d7726d9103b716
Dylan <dylan.oli@hotmail.com>
2021-10-20 23:13:43 -0300

    content

---
+++ planner/src/main/java/com/pw/planner/models/Content.java
@@ -0,0 +1,69 @@
+package com.pw.planner.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "content")
+public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String contentJson;
+    private boolean isLayout;
+
+    public Content() {
+    }
+
+    public Content(long id, String contentJson, boolean isLayout) {
+        this.id = id;
+        this.contentJson = contentJson;
+        this.isLayout = isLayout;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContentJson() {
+        return this.contentJson;
+    }
+
+    public void setContentJson(String contentJson) {
+        this.contentJson = contentJson;
+    }
+
+    public boolean isIsLayout() {
+        return this.isLayout;
+    }
+
+    public boolean getIsLayout() {
+        return this.isLayout;
+    }
+
+    public void setIsLayout(boolean isLayout) {
+        this.isLayout = isLayout;
+    }
+
+    public Content id(long id) {
+        setId(id);
+        return this;
+    }
+
+    public Content contentJson(String contentJson) {
+        setContentJson(contentJson);
+        return this;
+    }
+
+    public Content isLayout(boolean isLayout) {
+        setIsLayout(isLayout);
+        return this;
+    }
+}
---
+++ planner/src/main/java/com/pw/planner/repositories/ContentRepository.java
@@ -0,0 +1,8 @@
+package com.pw.planner.repositories;
+
+import com.pw.planner.models.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentRepository extends JpaRepository<Content, Long> {
+
+}
